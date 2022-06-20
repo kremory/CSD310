60be7f33fa73e386ec87733864bf22dd2c48a9c1
kremory <kemory@unomaha.edu>
2022-06-20 16:34:41 -0600

    'main'

--- Module_6/pytech_delete.py
+++ Module_6/pytech_delete.py
@@ -2,3 +2,22 @@ from pymongo import MongoClient
 url="mongodb+srv://admin:admin@cluster0.550ivtl.mongodb.net/?retryWrites=true&w=majority" 
 client = MongoClient(url)
 db = client.pytech
+collection=db.students
+cursor=collection.find()
+for record in cursor:
+
+    print(record)
+draco={
+    "first_name": "Draco",
+    "last_name": "Malfoy",
+    "student_id": "1010"
+}
+draco_student_id=collection.insert_one(draco).inserted_id
+doc=db.students.find_one({"student_id": "1010"})
+print(doc["student_id"])
+fordelete={"student_id": '1010'}
+collection.delete_one(fordelete)
+cursor=collection.find()
+for record in cursor:
+
+    print(record)
