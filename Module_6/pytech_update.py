139b51459577d60ce4eb71961d38252194b78d03
kremory <kemory@unomaha.edu>
2022-06-20 16:20:21 -0600

    'main'

--- Module_6/pytech_update.py
+++ Module_6/pytech_update.py
@@ -1,16 +1,15 @@
+from unittest import result
 from pymongo import MongoClient
 url="mongodb+srv://admin:admin@cluster0.550ivtl.mongodb.net/?retryWrites=true&w=majority" 
 client = MongoClient(url)
 db = client.pytech
 collection=db.students
-result = collection.update_one(
-    {"student_id": 1007},
-    {"$set":
-     {
-            "last_name": "Malfoy"
-            }
-            }
-            )
 cursor=collection.find()
 for record in cursor:
+
     print(record)
+newname= { "$set": {'last_name':'Malfoy'}}
+stud_id={'student_id':'1007'}
+result=collection.update_one(stud_id, newname)
+doc=collection.find_one({"student_id": "1007"})
+print(doc["last_name"])
