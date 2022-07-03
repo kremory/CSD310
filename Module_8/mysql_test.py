827da47d1e7bd8975e802cfd5c372721d459cfe9
kremory <kemory@unomaha.edu>
2022-07-03 09:37:42 -0600

    'main'

--- Module_8/mysql_test.py
+++ Module_8/mysql_test.py
@@ -1,22 +1,20 @@
 import mysql.connector
 from mysql.connector import errorcode
 config ={
-    "user" : "pysports user",
+    "user" : "root",
+    "password": "Romulos21!",
     "host" : "127.0.0.1",
     "database" : "pysports",
     "raise_on_warnings" : True
 }
 try:
-    db=mysql.connector.connct(**config)
-    print("\n Database user {} connected to MySQL on host {} with database {}".format(config{"user"}, config{"host"}, config{"database"}))
-
+    db=mysql.connector.connect(**config)
+    print("\n   Database user   {} connected to MySQL on host{} with database {}".format(config["user"], config["host"], config["database"]))
     input("\n\n Press any key to continue....")
-except mysql.connector.Erorr as err:
+except mysql.connector.Error as err:
     if err.errno== errorcode.ER_ACCESS_DENIED_ERROR:
         print(" the supplied username or password is invalid")
-    elif.err.errno == errorcode.ER_BAD_DB_ERRROR:
+    elif err.errno == errorcode.ER_BAD_DB_ERRROR:
         print("  The specified database does not exist")
     else:
         print(err)
-finally:
-    db.close()
